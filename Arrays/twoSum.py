84ba8787765458a9b67fbfbbc8bf27eb1bae577d
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-14 18:06:47 +0530

    Update twoSum.py

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -1,11 +1,11 @@
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-Solution : 1) Brute Force method with complexitties  O(n^2) Time & O(1) Space
+# Solution : 1) Brute Force method with complexitties  O(n^2) Time & O(1) Space
   
    def twoSum(self, nums: List[int], target: int) -> List[int]:
       for i in range(len(nums) - 1) :
