e87141a0215c68abb4c28c79fc135f542487e163
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-15 15:43:41 +0530

    Update nonConstructibleChange.py

--- Arrays/nonConstructibleChange.py
+++ Arrays/nonConstructibleChange.py
@@ -1,5 +1,7 @@
-# Given an array of positive integers representing the values of coins in your possession, write a function that returns the minimum amount of change (the minimum sum of money) that you cannot create. The given coins can have any positive integer value and aren't necessarily unique (.e., you can have multiple coins of the same value). 
-# For example, if you're given coins = [1, 2, 5], the minimum amount of change that you can't create is 4. If you're given no coins, the minimum amount of change that you can't create is 1.
+# Given an array of positive integers representing the values of coins in your possession, write a function that returns the minimum amount of change (the minimum sum of money) 
+# that you cannot create. The given coins can have any positive integer value and aren't necessarily unique (.e., you can have multiple coins of the same value). 
+# For example, if you're given coins = [1, 2, 5], the minimum amount of change that you can't create is 4. If you're given no coins, the minimum amount of change
+# that you can't create is 1.
 # Input [5, 7, 1, 1, 2, 3, 22] >> Output 20
 
 # Solution
