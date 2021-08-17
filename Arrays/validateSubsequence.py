0f5df5662502498e133048e3464da4620b982603
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-17 11:41:25 +0530

    a commit added

--- Arrays/validateSubsequence.py
+++ Arrays/validateSubsequence.py
@@ -1,3 +1,7 @@
 # Implement a function that takes two arrays of integers as input and finds whether all the numbers in the sequence array appear in the first array and they 
 # appear in the same order. In other words, the function need to find out if we can get the sequence array, when we delete some or no elements in the first 
 # array without changing the order of the remaining elements.
+# For example:
+# array = [5, 1, 22, 25, 6, -1, 8, 10];
+# sequence = [1, 6, -1, 10];
+# The output should be true.
