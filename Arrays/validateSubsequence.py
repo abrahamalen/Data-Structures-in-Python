8aec68a05179a1bcbecb34242354d71b936d7fa3
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-17 12:20:33 +0530

    another commit
    added solution using while loop

--- Arrays/validateSubsequence.py
+++ Arrays/validateSubsequence.py
@@ -5,3 +5,19 @@
 # array = [5, 1, 22, 25, 6, -1, 8, 10];
 # sequence = [1, 6, -1, 10];
 # The output should be true.
+
+# Solution
+# 1) Using while loop. O(n) Time & O(1) Space
+  
+  def isValidSubsequence(array, sequence):
+	arrayIdx = 0
+	seqIdx = 0
+	while arrayIdx < len(array) and seqIdx < len(sequence) :
+		if array[arrayIdx] == sequence[seqIdx] :
+			seqIdx += 1
+		arrayIdx += 1
+	return seqIdx == len(sequence)
+
+# 2) Using for loop. O(n) Time & O(1) Space
+   
+  
