298a284dbfffe67c9ccebbfab9d3a83660e07472
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-21 22:55:48 +0530

    another commit

--- Arrays/smallestDifference.py
+++ Arrays/smallestDifference.py
@@ -1,9 +1,9 @@
-Given two arrays of integers, I am asked to write a function that finds two numbers, where one number comes from the first array and another number comes 
-from the second array, with smallest absolute difference, and returns them in an array. 
-In the resulting array, the number from the first array should come first.
+# Given two arrays of integers, I am asked to write a function that finds two numbers, where one number comes from the first array and another number comes 
+# from the second array, with smallest absolute difference, and returns them in an array. 
+# In the resulting array, the number from the first array should come first.
 
-For example,
-arrayOne = [-1, 5, 10, 20, 28, 3];
-arrayTwo = [26, 134, 135, 15, 17];
+# For example,
+# arrayOne = [-1, 5, 10, 20, 28, 3];
+# arrayTwo = [26, 134, 135, 15, 17];
 
-The output is [28, 26].
+# The output is [28, 26].
