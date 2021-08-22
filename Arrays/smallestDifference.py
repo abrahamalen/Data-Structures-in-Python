d4e00362fea6d0eb40bcf9faa614c65665a0a5dc
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-22 10:35:51 +0530

    added the first solution using sorting

--- Arrays/smallestDifference.py
+++ Arrays/smallestDifference.py
@@ -5,5 +5,32 @@
 # For example,
 # arrayOne = [-1, 5, 10, 20, 28, 3];
 # arrayTwo = [26, 134, 135, 15, 17];
-
 # The output is [28, 26].
+
+# Solution
+# Using sorting
+# O(nlog(n) + mlog(m)) Time (where n & m are length of arrayOne and arrayTwo respectively) and O(1) Space complexity
+
+def smallestDifference(arrayOne, arrayTwo) :
+	arrayOne.sort()
+	arrayTwo.sort()
+	idxOne = 0
+	idxTwo = 0
+	smallest = float("inf")
+	current = float("inf")
+	smallestPair = []
+	while idxOne < len(arrayOne) and idxTwo < len(arrayTwo) :
+		firstNum = arrayOne[idxOne]
+		secondNum = arrayTwo[idxTwo]
+		if firstNum < secondNum :
+			current = secondNum - firstNum
+			idxOne += 1
+		elif secondNum < firstNum :
+			current = firstNum - secondNum
+			idxTwo += 1
+		else :
+			return [firstNum, secondNum]
+		if smallest > current :
+			smallest = current
+			smallestPair = [firstNum, secondNum]
+	return smallestPair
