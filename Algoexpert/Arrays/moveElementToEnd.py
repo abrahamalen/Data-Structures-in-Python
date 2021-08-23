6d2f166af581236149fe3ea24315e65c48a74a66
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-23 15:30:33 +0530

    First solution added

--- Algoexpert/Arrays/moveElementToEnd.py
+++ Algoexpert/Arrays/moveElementToEnd.py
@@ -11,3 +11,16 @@
   [1, 3, 4. 2, 2, 2, 2, 2]  // The numbers 1, 3 and 4 could be ordered differently.
   
   """
+  
+  # Solution
+  # Using swap concept. O(n) time & O(1) space.
+  def moveElementToEnd(array, toMove) :
+	i = 0
+	j = len(array) - 1
+	while i < j :
+		while i < j and array[j] == toMove :
+			j -= 1
+		if array[i] == toMove :
+			array[i], array[j] = array[j], array[i]
+		i += 1
+	return array
