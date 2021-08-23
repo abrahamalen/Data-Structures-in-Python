111cec0ab20c4b6715698133ae01896b09fe32aa
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-23 13:59:51 +0530

    Question nested inside double quotes.

--- Algoexpert/Arrays/twoNumberSum.py
+++ Algoexpert/Arrays/twoNumberSum.py
@@ -1,9 +1,11 @@
-# Given an array of integers nums and an integer target, return the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
-# You can return the answer in any order.
+"""
+Given an array of integers nums and an integer target, return the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
 
-# Input: nums = [2,7,11,15], target = 9
-# Output: [2, 7]
-# Output: Because nums[0] + nums[1] == 9, we return [2, 7].
+Sample Input: nums = [2,7,11,15], target = 9
+Sample Output: [2, 7]
+Explanation: Because nums[0] + nums[1] == 9, we return [2, 7].
+"""
 
 # Solutions
 # 1) Brute Force method with complexitties  
