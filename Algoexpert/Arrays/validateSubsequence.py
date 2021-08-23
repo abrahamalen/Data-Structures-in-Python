36e6f1a6637589060681943a13826756c749230f
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-23 14:00:40 +0530

    Question nested inside double quotes.

--- Algoexpert/Arrays/validateSubsequence.py
+++ Algoexpert/Arrays/validateSubsequence.py
@@ -1,10 +1,12 @@
-# Implement a function that takes two arrays of integers as input and finds whether all the numbers in the sequence array appear in the first array and they 
-# appear in the same order. In other words, the function need to find out if we can get the sequence array, when we delete some or no elements in the first 
-# array without changing the order of the remaining elements.
-# For example:
-# array = [5, 1, 22, 25, 6, -1, 8, 10];
-# sequence = [1, 6, -1, 10];
-# The output should be true.
+"""
+Implement a function that takes two arrays of integers as input and finds whether all the numbers in the sequence array appear in the first array and they 
+appear in the same order. In other words, the function need to find out if we can get the sequence array, when we delete some or no elements in the first 
+array without changing the order of the remaining elements.
+For example:
+array = [5, 1, 22, 25, 6, -1, 8, 10];
+sequence = [1, 6, -1, 10];
+The output should be true.
+"""
 
 # Solution
 # 1) Using while loop. O(n) Time & O(1) Space
