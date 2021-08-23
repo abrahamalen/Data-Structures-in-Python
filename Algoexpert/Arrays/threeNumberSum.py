35d05db6011be713883ae665bd30a6fd9263ae16
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-23 13:54:28 +0530

    Question nested inside double quotes.

--- Algoexpert/Arrays/threeNumberSum.py
+++ Algoexpert/Arrays/threeNumberSum.py
@@ -1,12 +1,13 @@
-# Given an array of distinct integers and an integer representing a target sum, write a function that find all combinations of three numbers in the array
-# that add up to the target sum. The function should return a 2-dimensional array of all these combinations. The numbers in each combination should be ordered 
-# in ascending order, and the combinations themselves should also be ordered in ascending order, for instance: [[-8, 2, 6],[-8, 3, 5], [-6, 1, 5]]. 
-# If there is no such combination, return an empty array.
-# Sample input -> array = [12, 3, 1, 2, -6, 5, -8, 6] , targetSum = 0
-# Output -> [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+"""
+Given an array of distinct integers and an integer representing a target sum, write a function that find all combinations of three numbers in the array
+that add up to the target sum. The function should return a 2-dimensional array of all these combinations. The numbers in each combination should be ordered 
+in ascending order, and the combinations themselves should also be ordered in ascending order, for instance: [[-8, 2, 6],[-8, 3, 5], [-6, 1, 5]]. 
+If there is no such combination, return an empty array.
+Sample input -> array = [12, 3, 1, 2, -6, 5, -8, 6] , targetSum = 0
+Output -> [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+"""
 
 # Solution
-
 # Using sorting O(n^2) Time and O(n) Space
 def threeNumberSum(array, targetSum) :
 	array.sort()
