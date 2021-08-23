ab08985f412d0be30631677b52ed4bc9ca10e09c
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-23 13:52:25 +0530

    Question nested inside double quotes.

--- Algoexpert/Arrays/smallestDifference.py
+++ Algoexpert/Arrays/smallestDifference.py
@@ -1,11 +1,13 @@
-# Given two arrays of integers, I am asked to write a function that finds two numbers, where one number comes from the first array and another number comes 
-# from the second array, with smallest absolute difference, and returns them in an array. 
-# In the resulting array, the number from the first array should come first.
+"""
+Given two arrays of integers, I am asked to write a function that finds two numbers, where one number comes from the first array and another number comes 
+from the second array, with smallest absolute difference, and returns them in an array. 
+In the resulting array, the number from the first array should come first.
+"""
 
-# For example,
+# Sample Input,
 # arrayOne = [-1, 5, 10, 20, 28, 3];
 # arrayTwo = [26, 134, 135, 15, 17];
-# The output is [28, 26].
+# Sample Output is [28, 26].
 
 # Solution
 # Using sorting
