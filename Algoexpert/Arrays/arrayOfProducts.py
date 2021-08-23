2fe1c83f31caa1e21a63308738561ce9b65cfe5c
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-23 13:49:58 +0530

    Question aligned

--- Algoexpert/Arrays/arrayOfProducts.py
+++ Algoexpert/Arrays/arrayOfProducts.py
@@ -1,11 +1,12 @@
-# Write a function that takes in a non-empty array of integers and returns an array of the same length, where each element in the output array is equal 
-# to the product of every other number in the input array.
-# In other words, the value at output[i] is equal to the product of every number in the input array other than input[i].
+"""
+Write a function that takes in a non-empty array of integers and returns an array of the same length, where each element in the output array is equal 
+to the product of every other number in the input array.
+In other words, the value at output[i] is equal to the product of every number in the input array other than input[i].
 
-# Sample input -> array = [5, 1, 4, 2] , Output -> [8, 40, 10, 20]
+Sample input -> array = [5, 1, 4, 2] , Output -> [8, 40, 10, 20]
+"""
 
 # Solution
-
 # 1) Using Brute force method iterating through nested for loops
 # O(n^2) time & O(n) space complexity
 
