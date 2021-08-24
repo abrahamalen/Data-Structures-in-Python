b1d2bb577c80e903b35d38ab4a539cbe8e059f0f
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-24 10:45:23 +0530

    Problem statement added

---
+++ Algoexpert/Arrays/monotonicArray.py
@@ -0,0 +1,9 @@
+"""
+Write a function that takes in an array of integers and returns a boolean representing whether the array is monotonic.
+An array is said to be monotonic if its elements, from left to right, are entirely non-increasing or entirely non-decreasing.
+Non-increasing elements aren't necessarily exclusively increasing; they simply don't decrease.
+Not that empty arrays and arrays of one element are monotonic.
+
+Sample Input: array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+Sample Output: true
+"""
