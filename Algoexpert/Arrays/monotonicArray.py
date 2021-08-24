13e822fd8de70b292d3ea5a8e8d03e8525c65e9a
Alen Abraham <53680255+Alenabraham07@users.noreply.github.com>
2021-08-24 15:33:40 +0530

    Added first solution

--- Algoexpert/Arrays/monotonicArray.py
+++ Algoexpert/Arrays/monotonicArray.py
@@ -7,3 +7,26 @@ Not that empty arrays and arrays of one element are monotonic.
 Sample Input: array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
 Sample Output: true
 """
+
+# Solution
+# 1) Solution which is somewhat complex and run with complexities O(n) Time & O(1) Space
+def isMonotonic(array):
+	if len(array) <= 2:
+		return True
+	direction = array[1] - array[0]
+	for i in range(2, len(array)):
+		if direction == 0:
+			direction = array[i] - array[i - 1]
+			continue
+		if breaksDirection(direction, array[i - 1], array[i]):
+			return False
+	return True
+
+def breaksDirection(direction, previousInt, currentInt):
+	difference = currentInt - previousInt
+	if direction > 0:
+		return difference < 0
+	return difference > 0
+
+# 2) Somewhat better and simpler code which is understandable and have O(n) Time & O(1) Space complexities.
+
