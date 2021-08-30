97333accf035644c1a366e21e896dd1bf2b3a696
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-08-30 15:09:51 +0530

    Second solution added
    Recursive method

--- Algoexpert/Arrays/spiralTraverse.py
+++ Algoexpert/Arrays/spiralTraverse.py
@@ -43,3 +43,31 @@ def spiralTraverse(array):
 		endCol -= 1
 	
 	return result
+
+# 2) Solution with recursive method. Complexities O(N) Time and O(N) Space where 'N' is the total no:of elements in the entire 2-D array.
+def spiralTraverse(array):
+	result = []
+	spiralFill(array, 0, len(array) - 1, 0, len(array[0]) - 1, result)
+	return result
+
+def spiralFill(array, startRow, endRow, startCol, endCol, result):
+	if startRow > endRow or startCol > endCol:
+		return
+	
+	for col in range(startCol, endCol + 1):
+		result.append(array[startRow][col])
+		
+	for row in range(startRow + 1, endRow + 1):
+		result.append(array[row][endCol])
+			
+	for col in reversed(range(startCol, endCol)):
+		if startRow == endRow:
+			break
+		result.append(array[endRow][col])
+			
+	for row in reversed(range(startRow + 1, endRow)):
+		if startCol == endCol:
+			break
+		result.append(array[row][startCol])
+		
+	spiralFill(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result)
