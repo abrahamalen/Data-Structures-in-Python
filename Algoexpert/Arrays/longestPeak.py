0cedae54e9082db96000e85265b8792d353cb78e
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-08-30 12:11:29 +0530

    another commit -added solution

--- Algoexpert/Arrays/longestPeak.py
+++ Algoexpert/Arrays/longestPeak.py
@@ -10,3 +10,27 @@ array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
 Sample output
 6 // 0, 10, 6, 5, -1, -3
 """
+
+# Solution
+# Complexity O(n) Time and O(1) Space.
+def longestPeak(array):
+	longestPeakLength = 0
+	i = 1
+	while i < len(array) - 1:
+		isPeak = array[i - 1] < array[i] and array[i] > array[i + 1]
+		if not isPeak:
+			i += 1
+			continue
+		
+		leftIdx = i - 2
+		while leftIdx >= 0 and array[leftIdx] < array[leftIdx + 1]:
+			leftIdx -= 1
+		rightIdx = i + 2
+		while rightIdx <  len(array) and array[rightIdx] < array[rightIdx - 1]:
+			rightIdx += 1
+			
+		currentPeakLength = rightIdx - leftIdx - 1
+		longestPeakLength = max(longestPeakLength, currentPeakLength)
+		i = rightIdx
+	
+	return longestPeakLength
