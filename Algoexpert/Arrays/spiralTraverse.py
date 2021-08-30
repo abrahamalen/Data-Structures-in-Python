f25585abad6a842d861eaac2e8c7ab5a90b7685c
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-08-30 13:03:56 +0530

    question updated

--- Algoexpert/Arrays/spiralTraverse.py
+++ Algoexpert/Arrays/spiralTraverse.py
@@ -5,7 +5,10 @@ Spiral order starts at the top left corner of the two-dimensional array, goes to
 pattern all the way until every element has been visited.
 
 Sample input
-array = [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7],]
+array = [[ 1,  2,  3, 4], 
+         [12, 13, 14, 5], 
+         [11, 16, 15, 6], 
+         [10,  9,  8, 7],]
 Sample output
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 """
