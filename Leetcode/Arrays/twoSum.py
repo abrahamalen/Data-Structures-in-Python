25e67ac4ef3a9728cf12085efda25933fd04e2e2
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2022-04-07 10:34:43 +0530

    brute force sol added

--- Leetcode/Arrays/twoSum.py
+++ Leetcode/Arrays/twoSum.py
@@ -14,8 +14,17 @@ Output: [1,2]
 Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
-"""
-# Solution
+"""  
+# Solution - Brute force  
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+           
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]  
+                
+# Solution - using dictionary
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         array = {}
