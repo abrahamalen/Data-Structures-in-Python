c960a11877035039020b42981895517484869c4c
abrahamalen <alenabraham4874@gmail.com>
2022-04-08 23:31:30 +0530

    added problems
    Signed-off-by: abrahamalen <alenabraham4874@gmail.com>

---
+++ Leetcode/Arrays/Easy/moveZeroes.py
@@ -0,0 +1,32 @@
+'''
+Move Zeroes - Easy
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+    1 <= nums.length <= 104
+    -231 <= nums[i] <= 231 - 1
+'''
+
+# Solution - Brute force
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for j in range(len(nums)):
+            if nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
